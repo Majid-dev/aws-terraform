579915a45dc2f66324211e9ad65ac3fbe6e2913c
Majid Karimi <majid0karimi@gmail.com>
2023-06-11 14:29:10 +0430

    add security group, vpc and key pair

--- EC2/entryscript.sh
+++ EC2/entryscript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-apt upgrade -y
-apt install -y apache2
-systemctl statrt apache2
-systemctl enable apache2
+sudo apt upgrade -y
+sudo apt install -y apache2
+sudo systemctl statrt apache2
+sudo systemctl enable apache2
 echo "<h1> Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
--- EC2/main.tf
+++ EC2/main.tf
@@ -14,11 +14,17 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
+resource "aws_key_pair" "ssh_key" {
+  key_name = "ssh_key"
+  public_key = file("id_rsa.pub")
+}
+
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
-  user_data = file("entryscript.sh")
-
+  user_data     = file("entryscript.sh")
+  vpc_security_group_ids = [ aws_security_group.allow_http_ssh.id ]
+  key_name = aws_key_pair.ssh_key.key_name
   # root disk
   root_block_device {
     volume_size           = "8"
@@ -28,7 +34,48 @@ resource "aws_instance" "web" {
   }
 
   tags = {
-    Name = "my_instance"
+    Name       = "my_instance"
     Department = "Finance"
+  }
+}
+
+resource "aws_default_vpc" "default" {
+  tags = {
+    Name = "Default VPC"
+  }
+}
+
+
+resource "aws_security_group" "allow_http_ssh" {
+  name        = "allow_http_ssh"
+  description = "Allow http and ssh inbound traffic"
+  vpc_id      = aws_default_vpc.default.id
+
+  ingress {
+    description = "http from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "ssh from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+   egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_http_ssh"
   }
 }
