1d2cd532ed405b4c87cd7f2380e643ccad534640
Majid Karimi <majid0karimi@gmail.com>
2023-06-17 14:13:43 +0430

    create auto scaling group

---
+++ ASG/main.tf
@@ -0,0 +1,168 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+
+# resource "aws_instance" "web" {
+#   for_each               = toset(var.instance_list)
+#   ami                    = data.aws_ami.ubuntu.id
+#   instance_type          = var.instance_type
+#   user_data              = file(var.entry_script_file)
+#   vpc_security_group_ids = [aws_security_group.allow_http.id]
+#   subnet_id              = aws_default_subnet.sub_az_1a.id
+#   tags = {
+#     Name = each.value
+#   }
+# }
+
+resource "aws_launch_template" "template" {
+  name_prefix   = "launch_template"
+  image_id      = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+  user_data = file(var.entry_script_file)
+  vpc_security_group_ids = [aws_security_group.allow_http.id]
+}
+
+resource "aws_autoscaling_group" "demo_asg" {
+  availability_zones = [var.az_1a]
+  desired_capacity   = 2
+  max_size           = 3
+  min_size           = 1
+
+  target_group_arns = [aws_lb_target_group.target_group.arn]
+
+  health_check_type = "ELB"
+  health_check_grace_period = 300
+
+  launch_template {
+    id      = aws_launch_template.template.id
+    version = "$Latest"
+  }
+
+}
+resource "aws_default_vpc" "default" {
+  tags = {
+    Name = "Default VPC"
+  }
+}
+
+resource "aws_default_subnet" "sub_az_1a" {
+  availability_zone = var.az_1a
+
+  tags = {
+    Name = "Default subnet for us-east-1a"
+  }
+}
+
+resource "aws_default_subnet" "sub_az_1b" {
+  availability_zone = var.az_1b
+
+  tags = {
+    Name = "Default subnet for us-east-1b"
+  }
+}
+
+resource "aws_security_group" "allow_http" {
+  name        = "allow_http"
+  description = "Allow http inbound traffic"
+  vpc_id      = aws_default_vpc.default.id
+
+  ingress {
+    description     = "http from VPC"
+    from_port       = 80
+    to_port         = 80
+    protocol        = var.gp_protocol
+    security_groups = [aws_security_group.sg_lb.id]
+  }
+
+  tags = {
+    Name = "allow_http"
+  }
+}
+
+resource "aws_security_group" "sg_lb" {
+  name        = "sg_elb"
+  description = "Allow http inbound traffic to load balancer"
+  vpc_id      = aws_default_vpc.default.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = var.gp_protocol
+    cidr_blocks = [var.cidr_blocks]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [var.cidr_blocks]
+  }
+
+  tags = {
+    Name = "security_group_elb"
+  }
+}
+
+resource "aws_lb" "alb" {
+  name               = "test-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.sg_lb.id]
+  subnets            = [aws_default_subnet.sub_az_1a.id, aws_default_subnet.sub_az_1b.id]
+
+  enable_deletion_protection = false
+
+  tags = {
+    Environment = "production"
+  }
+}
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target_group.arn
+  }
+}
+
+
+resource "aws_lb_target_group" "target_group" {
+  name        = "lb-tg-1"
+  port        = 80
+  protocol    = "HTTP"
+  protocol_version = "HTTP1"
+  target_type = "instance"
+  vpc_id      = aws_default_vpc.default.id
+
+  health_check {
+    path                = "/"
+    port                = 80
+    protocol            = "HTTP"
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+    matcher             = "200-499"
+  }
+}
+
+# resource "aws_lb_target_group_attachment" "tg_attach" {
+#   for_each         = toset(var.instance_list)
+#   target_group_arn = aws_lb_target_group.target_group.arn
+#   target_id        = aws_instance.web[each.value].id
+#   port             = 80
+# }
