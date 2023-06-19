111b3f299997928437cc303056bb5bba0f3da91e
Majid Karimi <majid0karimi@gmail.com>
2023-06-19 10:18:59 +0430

    add security group module

--- IAM/output.tf
+++
@@ -1,9 +0,0 @@
-
-output "aws_iam_user_secret" {
-  value     = aws_iam_access_key.access_key.secret
-  sensitive = true
-}
-
-output "password" {
-  value = aws_iam_user_login_profile.profile.password
-}
