2a7436b217472010429b1896ab6dc61948d2831b
Majid Karimi <majid0karimi@gmail.com>
2023-06-24 10:22:33 +0430

    add backup retention period to RDS

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -11,6 +11,7 @@ resource "aws_db_instance" "primary" {
   identifier              = var.db_instance_identifier
   engine                  = "mysql"
   engine_version          = "8.0"
+  backup_retention_period = 7
   instance_class          = var.instance_class
   allocated_storage       = var.allocated_storage
   db_name                 = var.db_name
@@ -28,11 +29,6 @@ resource "aws_db_instance" "primary" {
 resource "aws_db_instance" "read_replica" {
   count                   = var.create_read_replica ? 1 : 0
   identifier              = "${var.db_instance_identifier}-read-replica"
-  engine                  = "mysql"
-  engine_version          = "8.0"
-  instance_class          = var.instance_class
-  allocated_storage       = var.allocated_storage
-  skip_final_snapshot     = true
   vpc_security_group_ids  = var.vpc_security_group_ids
   db_subnet_group_name    = aws_db_subnet_group.db_subnet_group.name
   replicate_source_db     = aws_db_instance.primary.identifier
