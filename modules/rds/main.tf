55c0a278b6623d935b5b06dc2695bfd86604a94b
Majid Karimi <majid0karimi@gmail.com>
2023-06-24 12:34:57 +0430

    Modify read_replica resource

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -29,11 +29,15 @@ resource "aws_db_instance" "primary" {
 resource "aws_db_instance" "read_replica" {
   count                   = var.create_read_replica ? 1 : 0
   identifier              = "${var.db_instance_identifier}-read-replica"
+  engine                  = "mysql"
+  engine_version          = "8.0"
   backup_retention_period = 7
   instance_class          = var.instance_class
+  allocated_storage       = var.allocated_storage
+  username                = var.db_username
+  password                = var.db_password
   vpc_security_group_ids  = var.vpc_security_group_ids
   db_subnet_group_name    = aws_db_subnet_group.db_subnet_group.name
-  replicate_source_db     = aws_db_instance.primary.identifier
 
   tags = {
     Name = "ReadReplicaDBInstance"
