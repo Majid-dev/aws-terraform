95ee55e416ecb97bdaa1d1cd6b44e1638927a52a
Majid Karimi <majid0karimi@gmail.com>
2023-06-26 08:11:52 +0430

    add more subnets to vpc

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,5 +1,13 @@
+# environment variables
 variable "region" {}
-variable "vpc_cidr_block" {}
 variable "project_name" {}
-variable "subnet_az1_cidr_block" {}
-variable "subnet_az2_cidr_block" {}
+variable "environment" {}
+
+#vpc variables
+variable "vpc_cidr" {}
+variable "public_subnet_az1_cidr" {}
+variable "public_subnet_az2_cidr" {}
+variable "private_app_subnet_az1_cidr" {}
+variable "private_app_subnet_az2_cidr" {}
+variable "private_data_subnet_az1_cidr" {}
+variable "private_data_subnet_az2_cidr" {}
