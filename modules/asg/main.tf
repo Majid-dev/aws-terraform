6019d2c216a47b9fcedf31c886a42bc105568b38
Majid Karimi <majid0karimi@gmail.com>
2023-06-26 11:33:32 +0430

    update auto scaling group resource

--- modules/asg/main.tf
+++ modules/asg/main.tf
@@ -1,14 +1,12 @@
-data "aws_availability_zones" "available" {}
 
 resource "aws_autoscaling_group" "demo_asg" {
-    name = "${var.project_name}"
-  availability_zones = ["us-east-1a"]
+  name = "${var.project_name}-asg"
   desired_capacity   = var.desired_capacity
   max_size           = var.max_size
   min_size           = var.min_size
 
   target_group_arns = [var.target_group_arn]
-
+  vpc_zone_identifier  = var.subnet_ids
   health_check_type = "ELB"
   health_check_grace_period = 300
 
